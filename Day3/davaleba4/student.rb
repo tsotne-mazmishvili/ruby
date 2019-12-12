a78f644de5c9646bf3fe39430a3876c49ab6bd05
tsotne1 <32372745+tsotne1@users.noreply.github.com>
2019-12-12 23:36:16 +0400

    .

---
+++ Day3/davaleba4/student.rb
@@ -0,0 +1,12 @@
+class Student
+  attr_accessor :name, :last_name, :gpa, :debt
+
+  def initialize(name, last_name, gpa, debt)
+    @name = name
+    @last_name = last_name
+    @gpa = gpa
+    @debt = debt
+  end
+
+
+end
