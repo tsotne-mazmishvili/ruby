f3c622885d6369cf7a8d357788e9bf52a62a933d
tsotne1 <32372745+tsotne1@users.noreply.github.com>
2020-01-13 02:06:10 +0400

    tic_tac_toe final

--- Day5/tic_tac_toe.rb
+++ Day5/tic_tac_toe.rb
@@ -1,62 +1,156 @@
-def tic_tac_toe(player1, player2)
-  turn = ""
-  turn_counter = 0
-  winners = [[1,2,3], [4,5,6], [7,8,9], [1,4,7], [2,5,8], [3,6,9], [1,5,9], [3,5,7]]
-  player1_combination = []
-  player2_combination = []
+def check_win(table, symbol, turn_counter, min_move_for_win, table_dimensions)
+  win = false
+  return win if turn_counter < min_move_for_win #დაიწყოს შემოწმება როცა სვლების რაოდენობა მოგების საშუალებას იძლევა
 
-  values = [1,2,3,4,5,6,7,8,9]
-  table = "[#{values[0]}] [#{values[1]}] [#{values[2]}]\n[#{values[3]}] [#{values[4]}] [#{values[5]}]\n[#{values[6]}] [#{values[7]}] [#{values[8]}]"
-  puts table
-
-  while  turn_counter < 9
-      even_odd = turn_counter % 2
-      #if even player1's turn
-      puts "#{player1}'s turn" if even_odd == 0
-      puts "#{player2}'s turn" if even_odd == 1
-
-      puts "choose number "
-      recieved_value = gets.chomp().to_i
-      puts "------------------"
-
-      #check if number is already used
-      if (1..9).include?(recieved_value) == false || values[recieved_value - 1] != recieved_value
-        puts "!!number is already used or wrong character!!"
-        redo
+  #ჰორიზონტალური ხაზების შემოწმება
+  (0...table.length).step(table_dimensions) do |index|
+    (table_dimensions).times do
+      if table[index] == symbol
+        win = true
+        index += 1
+      else
+        win = false
+        break
       end
-
-      if even_odd == 0
-        values[recieved_value - 1], turn = "X", player1
-        player1_combination << recieved_value
-      elsif even_odd == 1
-        values[recieved_value - 1], turn = "O", player2
-        player2_combination << recieved_value
-      end
-
-      table = "[#{values[0]}] [#{values[1]}] [#{values[2]}]\n[#{values[3]}] [#{values[4]}] [#{values[5]}]\n[#{values[6]}] [#{values[7]}] [#{values[8]}]"
-
-      if turn_counter > 3
-        winners.each do |x|
-          return "Winner is: #{player1}" if (x - player1_combination).empty?
-          return "Winner is: #{player2}" if (x - player2_combination).empty?
-        end
-      end
-
-      puts table
-      turn_counter += 1
-
     end
-    return "tie"
+    return win if win
+  end
+
+  #ვერტიკალური ხაზების შემოწმება
+  (0...table_dimensions).each do |index|
+    (table_dimensions).times do
+      if table[index] == symbol
+        win = true
+        index += table_dimensions
+      else
+        win = false
+        break
+      end
+    end
+    return win if win
+  end
+
+  #დიაგონალური(მარცხნიდან მარჯვნივ) ხაზის შემოწმება
+  (0...table.length).step(table_dimensions + 1) do |index|
+    if table[index] == symbol
+      win = true
+    else
+      win = false
+      break
+    end
+  end
+  return win if win
+
+  #დიაგონალური(მარჯვნიდან მარცხნივ) ხაზის შემოწმება
+  (table_dimensions - 1...table.length - (table_dimensions - 1)).step(table_dimensions - 1) do |index|
+    if table[index] == symbol
+      win = true
+    else
+      win = false
+      break
+    end
+  end
+  return win if win
 end
 
+
+#სვლის გაკეთება მაგიდაზე
+def make_move(table, table_visual, symbol, number)
+  #ჰარების რაოდენობა კვადრატულ ფრჩხილებში([]) სიმეტრიის შესანარჩუნებლად
+  spacing = number.length - symbol.length
+
+  #ჰარების ჩასმის ლოგიკა ფრჩხილებს შორის სიმეტრიულად
+  symbol_with_spacing = " " * (spacing - spacing / 2) + symbol.to_s + " " * (spacing / 2)
+
+  table[number.to_i - 1] = symbol
+  table_visual.sub(number, symbol_with_spacing)
+end
+
+
+#ამოწმებს შემოყვანილი რიცხვის ვალიდურობას
+def validate_number(number, used_numbers, biggest_number)
+  if (1..biggest_number).include?(number) == false  || used_numbers.include?(number)
+    return false
+  end
+  true
+end
+
+
+def define_player_turn(turn_counter, player1, player2)
+  if turn_counter % 2 == 0 #რადგან თამაშს იწყებს player1 ლუწი სვლები მისია
+    return player1
+  end
+  player2
+end
+
+
+#მიღებული მასივისგან(table) ამზადებს მაგიდის ვიზუალს დასაბეჭდათ
+def table_visualisation(table_dimensions, biggest_number, table)
+  visualised_table = ""
+
+  table.each do |number|
+    #ჰარების რაოდენობა კვადრატულ ფრჩხილებში([]) სიმეტრიის შესანარჩუნებლად
+    spacing = biggest_number.to_s.length - number.to_s.length
+
+    #ჰარების ჩასმის ლოგიკა ფრჩხილებს შორის სიმეტრიულად
+    visualised_table += "[" + (" " * (spacing - spacing / 2)) + number.to_s + (" " * (spacing / 2)) + "] "
+    visualised_table += "\n\n" if number % table_dimensions == 0
+  end
+  return visualised_table
+end
+
+
+def user_interface(table_dimensions = 3)
+  puts "|===============| Tic Tac Toe |===============|"
+
+  puts "player1: "
+  player1 = {"name" => gets.chomp(), "symbol" => "X"}
+  puts "\nplayer2: "
+  player2 = {"name" => gets.chomp(), "symbol" => "O"}
+
+  turn_counter = 0
+  max_possible_turn = table_dimensions ** 2
+  biggest_number = max_possible_turn
+  min_move_for_win = table_dimensions * 2 - 2
+
+  table = (1..(biggest_number)).to_a
+  table_visual = table_visualisation(table_dimensions, biggest_number, table)
+
+  used_numbers = []
+
+  puts "\n|--------| Game starts, good luck |--------|\n"
+  while turn_counter < max_possible_turn
+    puts table_visual
+
+    turn = define_player_turn(turn_counter, player1, player2)
+
+    puts turn["name"] + " chooses number: "
+    recieved_number = gets.chomp().to_i
+
+    puts "-----------------------------------------------\n"
+
+    if validate_number(recieved_number, used_numbers, max_possible_turn)
+      table_visual = make_move(table, table_visual, turn["symbol"], recieved_number.to_s)
+
+      if check_win(table, turn["symbol"], turn_counter, min_move_for_win, table_dimensions)
+        puts table_visual
+        return "Winner is " + turn["name"] + ", ***congratulations*** "
+      end
+
+      used_numbers << recieved_number
+    else
+      puts "\n!!!Given number is already used or it is not number. try again!!!"
+      redo
+    end
+    turn_counter += 1
+  end
+
+  return "\nGame ends in tie"
+end
+
+
 def main
-  puts "player1 name: "
-  player1 = gets.chomp()
-  puts "player2 name: "
-  player2 = gets.chomp()
-
-  puts tic_tac_toe(player1, player2)
-
+  puts user_interface()
 end
 
 main
